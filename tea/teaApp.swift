4398ee12ba6d5fcad1481ca11dfbfc6dba19763a
Mohammed Sayed <mohamadsayed7070@gmail.com>
2022-10-20 05:31:00 +0200

    Initial Commit

---
+++ tea/teaApp.swift
@@ -0,0 +1,17 @@
+//
+//  teaApp.swift
+//  tea
+//
+//  Created by Syber Expertise on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct teaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
